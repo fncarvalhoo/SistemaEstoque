7435148afa0503f073b2b2d7883c532a7390cefa
Fernando Carvalho <fernandocarvalho2304@gmail.com>
2023-01-26 19:23:56 -0300

    Category Model
    Criada a classe modelo de categoria.

---
+++ estoque/src/main/java/com/project/estoque/model/Category.java
@@ -0,0 +1,50 @@
+package com.project.estoque.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+
+    @Column(nullable = false, length = 40)
+    private String name;
+
+    @Column(nullable = false, length = 200)
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Category [id=" + id + ", name=" + name + ", description=" + description + "]";
+    }
+
+}
